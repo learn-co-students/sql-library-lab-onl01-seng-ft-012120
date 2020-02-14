a073d80ae15a689ff9e3b1e469f20eb9a1fcbb7c
reddevilcero <reddevil_cero@hotmail.com>
2020-02-14 11:42:12 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series (title, author_id, subgenre_id ) VALUES ('title 1', 2, 3 ), ('title2', 4, 8);
+
+INSERT INTO books (title, year, series_id) VALUES
+('random_title', '2015', 4),
+('random_title', '2015', 4),
+('random_title', '2015', 4),
+('random_title', '2015', 4),
+('random_title', '2015', 4),
+('random_title', '2015', 4);
+
+INSERT INTO characters (name, motto, species, author_id) VALUES
+('random name', 'random motto', 'no specie', 5),
+('random name', 'random motto', 'no specie', 5),
+('random name', 'random motto', 'no specie', 5),
+('random name', 'random motto', 'no specie', 5),
+('random name', 'random motto', 'no specie', 5),
+('random name', 'random motto', 'no specie', 5),
+('random name', 'random motto', 'no specie', 5),
+('random name', 'random motto', 'no specie', 5);
+
+INSERT INTO subgenres (name) VALUES ('name'), ('loto');
+
+INSERT INTO authors (name) VALUES ('me'), ('yopou');
+
+INSERT INTO character_books (character_id, book_id) VALUES
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2),
+(1,2);
